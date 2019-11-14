b1f5f0c1f1d32a6ca04aec2530fe60e0a1ba8474
kkbo5222 <kkbo5222@gmail.com>
2019-11-14 16:09:35 +0900

    loop

--- Node.JS/JavaScript_basic/basic_grammer.js
+++ Node.JS/JavaScript_basic/basic_grammer.js
@@ -219,7 +219,7 @@ console.log(result);
 
 // ex 4-2 | 조건문 - switch
 // 논리연산이 아닌 값의 동일 여부로 분기를 나눔
-
+/*
 var a = 1;
 var result = '';
 
@@ -236,3 +236,64 @@ switch(a) {
 }
 
 console.log(result);
+*/
+
+
+// ex 5 | 반복문
+
+// ex 5-1 | 반복문 - for
+/*
+var array = new Array();
+
+// 초기화 조건 연산
+for (var i = 0; i < 10; i++) {
+    // 배열에 1~9 추가
+	array.push(i);
+}
+
+console.log(array.toString());          // "0,1,2,3,4,5,6,7,8,9"
+*/
+
+
+// ex 5-2 | for -in
+/*
+var a = ['a', 'b', 'c', 'x', 'y', 'z'];
+var result = '';
+
+for (var i in a) {
+	result += 'index:'+ i + ', value:'+ a[i] + '\n'; 
+}
+
+console.log(result);
+
+// "index:0, value:a
+// index:1, value:b
+// index:2, value:c
+// index:3, value:x
+// index:4, value:y
+// index:5, value:z"
+*/
+
+
+// ex 5-3 | while
+/*
+//초기값
+var i = 0;
+//조건식
+while(i < 10) {
+    //증감식
+    i++;
+}
+
+console.log(i);
+*/
+
+// ex 5-4 | do-while
+// 최소한 do는 실행
+var i = 0; 
+
+do {
+  i++;
+} while (i < 10)
+  
+console.log(i)      // 10
