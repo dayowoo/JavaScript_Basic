9f9ae1427528de585b4a8a4b69bedecbf7d2cebf
kkbo5222 <kkbo5222@gmail.com>
2019-11-28 21:23:11 +0900

    Class, constructor, Super

---
+++ JS_for_React/Class_Super.js
@@ -0,0 +1,46 @@
+class Animal {
+    constructor(leg) {
+        this.leg = leg
+    }
+
+    printAnimal() {
+        console.log(this.name+"은 "+String(this.leg)+"개 다리를 가진다.")
+    }
+}
+
+// Lion -> Animal 유용한 기능들 가져다 쓰기
+class Lion extends Animal {
+    // 초기 lion 객체가 어떤 값을 가지는 지 설정
+    constructor(name, leg) {
+        // 부모의 input (constructor의 input)
+        super(leg)
+        this.name = name
+    }
+    getName() {
+        console.log("내 이름은 "+this.name)
+    }
+}
+
+myLion = new Lion("King", 4)
+myLion.getName()
+myLion.printAnimal()
+
+
+// Class & Super Quiz
+
+class Timer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { seconds: 0};
+    }
+
+    tick() {
+        this.setState(state => ({
+            seconds: state.seconds +1
+        }));
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+}
